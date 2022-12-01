33411948b8c331ed0708df72eef572fcab23a383
Patricio Olivares <paolivaresr@gmail.com>
2022-12-01 18:47:35 -0400

    Actualización clase 4-JS

--- 4-JS/Ej-ListPeople/js/scripts.js
+++ 4-JS/Ej-ListPeople/js/scripts.js
@@ -1,3 +1,5 @@
+
+/* Descomentar para probar eventos sin onclick */
 // window.onload = function(){
 //     var  boton = document.getElementById('boton');
 //     boton.addEventListener('click',function(){
@@ -6,11 +8,11 @@
 //         alert("Nombre ingresado");
 //     })
 // }
+
+
+/* Descomentar para probar eventos con onclick */
 function addName(){
-    var  boton = document.getElementById('boton');
-    boton.addEventListener('click',function(){
-        var name = document.getElementById("nombre").value;
-        document.getElementById("listado").innerHTML += "<li>" + name + "</li>";
-        alert("Nombre ingresado");
-    })
+    var name = document.getElementById("nombre").value;
+    document.getElementById("listado").innerHTML += "<li>" + name + "</li>";
+    alert("Nombre ingresado");
 }
