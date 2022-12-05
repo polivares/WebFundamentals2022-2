6db5efdd4aeaf018a937565f2421434cb99ee50d
Patricio Olivares <paolivaresr@gmail.com>
2022-12-05 19:19:50 -0400

    Última actualización clase 4-JS

--- 4-JS/Ej-ListPeople/js/scripts.js
+++ 4-JS/Ej-ListPeople/js/scripts.js
@@ -1,18 +1,27 @@
-
-/* Descomentar para probar eventos sin onclick */
-// window.onload = function(){
-//     var  boton = document.getElementById('boton');
-//     boton.addEventListener('click',function(){
-//         var name = document.getElementById("nombre").value;
-//         document.getElementById("listado").innerHTML += "<li>" + name + "</li>";
-//         alert("Nombre ingresado");
-//     })
-// }
-
-
 /* Descomentar para probar eventos con onclick */
 function addName(){
     var name = document.getElementById("nombre").value;
     document.getElementById("listado").innerHTML += "<li>" + name + "</li>";
     alert("Nombre ingresado");
 }
+
+
+/* Descomentar para probar eventos sin onclick */
+/* Código con etiqueta script del todo arriba */
+window.onload = function(){
+    var  boton = document.getElementById('boton');
+    boton.addEventListener('click',function(){
+        var name = document.getElementById("nombre").value;
+        document.getElementById("listado").innerHTML += "<li>" + name + "</li>";
+        alert("Nombre ingresado");
+    })
+}
+
+/* Código con etiqueta script del todo abajo*/
+// var  boton = document.getElementById('boton');
+// boton.addEventListener('click',function(){
+//     var name = document.getElementById("nombre").value;
+//     document.getElementById("listado").innerHTML += "<li>" + name + "</li>";
+//     alert("Nombre ingresado");
+// })
+
